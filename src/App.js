ba9943ecb3df4565de0fea06ebff6e366104166c
kotatabe <qwerty789kota@gmail.com>
2021-11-19 20:47:56 +0900

    station 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 /**
@@ -8,9 +9,24 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+
+  const [dogUrl, setDogUrl] = useState([]);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => setDogUrl(data.message))
+  }, [count]);
+
   return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
+    <body>
+      <header>
+        <h1>Dog App</h1>
+      </header>
+      <p>this is a dog</p>
+      <img src={dogUrl} alt="Dog" />
+      <button onClick={() => setCount(count + 1) }>更新</button>
+    </body>
   )
 }
